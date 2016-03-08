c9a1a2bbe83cea75bc19e4dcb7925152c776c8d9
Dennis Birch <pwsupport@pandaware.com>
2016-03-08 08:35:59 -0800

    Initial Commit

---
+++ Acronyms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acronyms
+//
+//  Created by Dennis Birch on 3/8/16.
+//  Copyright © 2016 Dennis Birch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acronyms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Acronyms
+//
+//  Created by Dennis Birch on 3/8/16.
+//  Copyright © 2016 Dennis Birch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
